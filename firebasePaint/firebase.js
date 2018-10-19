5d605230df6994b46a77cf262ae4d05fca72e7d3
thorcc <thorchrcow@gmail.com>
2018-10-19 17:04:16 +0200

    firebasePaint

---
+++ firebasePaint/firebase.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDYBCsMMhsdqyISZfkkPVWuE8_lWISk47U",
+    authDomain: "fir-paint-3d679.firebaseapp.com",
+    databaseURL: "https://fir-paint-3d679.firebaseio.com",
+    projectId: "fir-paint-3d679",
+    storageBucket: "",
+    messagingSenderId: "538477314582"
+};
+firebase.initializeApp(config);
+
+
+
