62e070a382ffd2cf85aa2a162cec8d9601069bcc
thorcc <thorchrcow@gmail.com>
2018-11-02 10:20:46 +0100

    add js

--- IT2/Fredag/trommemaskin/script.js
+++ IT2/Fredag/trommemaskin/script.js
@@ -1,5 +1,20 @@
 console.log("Skriptet er lastet");
 
 window.onkeydown = function(event){
-    console.log(event);
+    const boks = document.querySelector(`div[data-code=${event.code}]`);
+    const lyd = document.querySelector(`audio[data-code=${event.code}]`);
+
+    if(!lyd){
+        return;
+    }
+
+    lyd.currentTime = 0;
+    lyd.play();
+
+    boks.classList.add("spiller");
+
+}
+window.onkeyup = function(event){
+    const boks = document.querySelector(`div[data-code=${event.code}]`);
+    boks.classList.remove("spiller");
 }
