03c75fc00d57514e21dd67ab6cb1f020ca9a34a2
thorcc <thorchrcow@gmail.com>
2019-04-04 10:00:15 +0200

    kode fra timen

--- IT2/Objektorientert programmering/bubble-start/script.js
+++ IT2/Objektorientert programmering/bubble-start/script.js
@@ -1,38 +1,78 @@
-  // Referanser til HTML-elementer
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
+// Referanser til HTML-elementer
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// Klassedefinisjoner
+class Figur{
+    constructor(x,y,r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    beveg(){
+        this.x = this.x + Math.random()*10 - 5;
+        this.y = this.y + Math.random()*10 - 5;
+    }
+    tegn(){
+        ctx.strokeStyle = "white";
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.r,0,Math.PI*2);
+        ctx.stroke();
+    }
+}
+
+class Bubble extends Figur{
+    constructor(x,y,r){
+        super(x,y,r);
+    }
+}
+
+class Firkant extends Figur{
+    constructor(x,y,h,b){
+        super(x,y);
+        this.h = h;
+        this.b = b;
+    }
+    tegn(){
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.x,this.y,this.r,this.r);
+    }
+}
 
 
-  // Globale variabler
-  let bubble = {
-    x: 200,
-    y: 200
-  }
+// Globale variabler
+let boble1 = new Bubble(100,200,50);
+let boble2 = new Bubble(100,200,50);
+let firkant = new Firkant(50,60,40,100);
 
-  // Oppstart
-  gameloop();
+// Oppstart
+gameloop();
 
-  //Funksjonsdefinisjoner
-  function gameloop(){
+//Funksjonsdefinisjoner
+function gameloop(){
     tegnBakgrunn();
-    tegnBubble();
-    bevegBubble();
+    boble1.beveg();
+    boble1.tegn();
+    boble2.beveg();
+    boble2.tegn();
+    firkant.beveg();
+    firkant.tegn();
     requestAnimationFrame(gameloop);
-  }
+}
 
-  function tegnBakgrunn(){
+function tegnBakgrunn(){
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height); // Tegner bakgrunnsfarge på canvaset
-  }
+}
 
-  function tegnBubble(){
+function tegnBubble(){
     ctx.strokeStyle = "white";
     ctx.beginPath();
     ctx.arc(bubble.x, bubble.y, 50, 0, 2 * Math.PI);
     ctx.stroke()
-  }
+}
 
-  function bevegBubble(){
+function bevegBubble(){
     bubble.x = bubble.x + Math.random()*10 - 5;
     bubble.y = bubble.y + Math.random()*10 - 5;
-  }
+}
