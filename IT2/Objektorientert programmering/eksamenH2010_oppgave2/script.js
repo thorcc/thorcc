8eed951c275d4bc9e9a81ad3528037f5372a2615
thorcc <thorchrcow@gmail.com>
2019-04-05 12:35:57 +0200

    Update script.js

--- IT2/Objektorientert programmering/eksamenH2010_oppgave2/script.js
+++ IT2/Objektorientert programmering/eksamenH2010_oppgave2/script.js
@@ -4,14 +4,35 @@ console.log("Skriptet er lastet");
 const velgHotell = document.querySelector(".velgHotell");
 const valgtHotell = document.querySelector("#valgtHotell");
 
+
 // Klassedefinisjoner
 class Hotell{
-  constructor(navn,tlf,epost,bilde){
+  constructor(navn,tlf,epost,bilde,temp){
     this.navn = navn;
     this.tlf = tlf;
     this.epost = epost;
     this.bilde = bilde;
+    this.temp = temp;
   }
+
+  gjSnitt(){
+    let total = 0;
+    for(let tall of this.temp){
+      total += tall;
+    }
+    return total/this.temp.length;
+  }
+
+  maks(){
+    let maks = -Infinity;
+    for(let tall of this.temp){
+      if(tall > maks){
+        maks = tall;
+      }
+    }
+    return maks;
+  }
+
   vis(){
     let html = `
       <div>
@@ -19,6 +40,8 @@ class Hotell{
         <img src=${this.bilde}>
         <p>tlf: ${this.tlf}</p>
         <p>epost: ${this.epost}</p>
+        <p>Temp. siste 7: ${this.temp.join(", ")}</p>
+        <p>Gj. snitt: ${this.gjSnitt().toFixed(1)}</p>
       </div>
     `;
     valgtHotell.innerHTML = html;
@@ -26,8 +49,8 @@ class Hotell{
 }
 
 // Globale variabler
-let spidsbergseter = new Hotell("Spidsbergseter","61 28 40 00","post@sgh.no","Filer2010Hoteller/spidsbergseter.jpg");
-let rondablikk = new Hotell("Rondablikk","61 29 18 18","post@rondablikk.no","Filer2010Hoteller/rondablikk.jpg");
+let spidsbergseter = new Hotell("Spidsbergseter","61 28 40 00","post@sgh.no","Filer2010Hoteller/spidsbergseter.jpg",[-5,-3,-2,0,2,3]);
+let rondablikk = new Hotell("Rondablikk","61 29 18 18","post@rondablikk.no","Filer2010Hoteller/rondablikk.jpg",[-8,-5,-5,-3,-3,-3]);
 
 let hoteller = [spidsbergseter,rondablikk];
 
